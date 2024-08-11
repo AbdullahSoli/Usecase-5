4d557691b800be045a227d89f0f0d6f63d344442
AbdullahSoli <113032879+AbdullahSoli@users.noreply.github.com>
2024-08-11 23:21:43 +0300

    Update streamlit_Usecase5.py

--- streamlit_Usecase5.py
+++ streamlit_Usecase5.py
@@ -1,13 +1,21 @@
 import streamlit as st
 
-st.write("Hi")
+
 
 st.html(
-    "<h1>         !! هل امتلاك فيلا في الرياض مستحيل         </h1>"
-  
+    "<h2>         تبي وظيفة ؟؟         </h2>"
+    "<p>معك عبدالله و انا حديث تخرج و ابحث عن و ظيفة مثلك  و اثناء البحث قليت ليش ما افهم سوق العمل عندنا في السعودية فرحت و حللت بيانات عن السوق فتعال معي وشوف سوق العمل عندنا كيف </p>"
+)
+st.html(
+   "<h4>         اعلى منطقة في التوظيف         </h4>"
+    "<p>في البداية لازم نعرف وين نبحث عن وظايف و هنا اعطيناك وش هي اعلى منطقة في الوظايف   </p>"
+)
+st.image("Q.1.png",caption="Sunrise by the mountains")
+st.html(
+   
+    "<p>و مثل ما تلاحظ عزيزي القارى ان الرياض هي اعلى منطقة و هذا يعطي افضلية لأهل الرياض في الوظايف   </p>"
 )
 
-st.image("Q.1.png",caption="Sunrise by the mountains")
 st.image("Q.2.png",caption="Sunrise by the mountains")
 st.image("Q.3.png",caption="Sunrise by the mountains")
 st.image("Q.4.png",caption="Sunrise by the mountains")
