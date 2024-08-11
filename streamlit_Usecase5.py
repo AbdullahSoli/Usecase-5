65d6703faf8960010a13fc2a33dd62387c9ec61f
AbdullahSoli <113032879+AbdullahSoli@users.noreply.github.com>
2024-08-11 23:11:21 +0300

    Update streamlit_Usecase5.py

--- streamlit_Usecase5.py
+++ streamlit_Usecase5.py
@@ -8,8 +8,8 @@ st.html(
 )
 
 st.image("Q.1.png",caption="Sunrise by the mountains")
-st.image("https://github.com/AbdullahSoli/Usecase-5/blob/main/Q.2.png",caption="Sunrise by the mountains")
-st.image("https://github.com/AbdullahSoli/Usecase-5/blob/main/Q.3.png",caption="Sunrise by the mountains")
-st.image("https://github.com/AbdullahSoli/Usecase-5/blob/main/Q.4.png",caption="Sunrise by the mountains")
-st.image("https://github.com/AbdullahSoli/Usecase-5/blob/main/Q.5.png",caption="Sunrise by the mountains")
-st.image("https://github.com/AbdullahSoli/Usecase-5/blob/main/Q.6.png",caption="Sunrise by the mountains")
+st.image("Q.2.png",caption="Sunrise by the mountains")
+st.image("Q.3.png",caption="Sunrise by the mountains")
+st.image("Q.4.png",caption="Sunrise by the mountains")
+st.image("Q.5.png",caption="Sunrise by the mountains")
+st.image("Q.6.png",caption="Sunrise by the mountains")
